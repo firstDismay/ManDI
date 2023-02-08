8bc1e9a60237a3797212a0588b9145b60db37a41
Dmitry Ivanov <firstdismay@gmail.com>
2023-02-08 15:45:15 +0500

    feat: Добавлены команды для работы с позициями.

---
+++ command/position/add/position_add.cs
@@ -0,0 +1,88 @@
+﻿using ManDI.command.common;
+using Npgsql;
+using NpgsqlTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManDI.command.position.add
+{
+    /// <summary>
+    /// Команда добавляет позицию
+    /// </summary>
+    public class position_add : IParametersFunction
+    {
+        /// <summary>
+        /// Идентификатор родительской позиции
+        /// </summary>
+        public long iid_parent { get; set; }
+
+        /// <summary>
+        /// Идентификатор шаблона позиции
+        /// </summary>
+        public long iid_pos_temp { get; set; }
+
+        /// <summary>
+        /// Наименование позиции
+        /// </summary>
+        public string iname { get; set; }
+
+        /// <summary>
+        /// Описание позиции
+        /// </summary>
+        public string idesc { get; set; }
+
+        /// <summary>
+        /// Ключ сортировки
+        /// </summary>
+        public int isort { get; set; }
+
+        /// <summary>
+        /// Список параметров функции
+        /// </summary>
+        public IEnumerable<NpgsqlParameter> Parameters
+        {
+            get
+            {
+                NpgsqlParameter Parameter;
+                List<NpgsqlParameter> ListParameter = new List<NpgsqlParameter>();
+
+                Parameter = new NpgsqlParameter("iid_parent", NpgsqlDbType.Bigint);
+                Parameter.Value = iid_parent;
+                ListParameter.Add(Parameter);
+
+                Parameter = new NpgsqlParameter("iid_pos_temp", NpgsqlDbType.Bigint);
+                Parameter.Value = iid_pos_temp;
+                ListParameter.Add(Parameter);
+
+                Parameter = new NpgsqlParameter("iname", NpgsqlDbType.Varchar);
+                Parameter.Value = iname;
+                ListParameter.Add(Parameter);
+
+                Parameter = new NpgsqlParameter("idesc", NpgsqlDbType.Varchar);
+                Parameter.Value = idesc;
+                ListParameter.Add(Parameter);
+
+                Parameter = new NpgsqlParameter("isort", NpgsqlDbType.Integer);
+                Parameter.Value = isort;
+                ListParameter.Add(Parameter);
+
+                return ListParameter;
+            }
+        }
+
+        /// <summary>
+        /// Наименование функции API
+        /// </summary>
+        public string NameFunction
+        {
+            get
+            {
+                return "position_add";
+            }
+        }
+    }
+}
---
+++ command/position/upd/position_add.cs
@@ -0,0 +1,79 @@
+﻿using ManDI.command.common;
+using Npgsql;
+using NpgsqlTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManDI.command.position.upd
+{
+    /// <summary>
+    /// Команда обновляет позицию
+    /// </summary>
+    public class position_upd : IParametersFunction
+    {
+        /// <summary>
+        /// Идентификатор позиции
+        /// </summary>
+        public long iid { get; set; }
+        
+        /// <summary>
+        /// Наименование позиции
+        /// </summary>
+        public string iname { get; set; }
+
+        /// <summary>
+        /// Описание позиции
+        /// </summary>
+        public string idesc { get; set; }
+
+        /// <summary>
+        /// Ключ сортировки
+        /// </summary>
+        public int isort { get; set; }
+
+        /// <summary>
+        /// Список параметров функции
+        /// </summary>
+        public IEnumerable<NpgsqlParameter> Parameters
+        {
+            get
+            {
+                NpgsqlParameter Parameter;
+                List<NpgsqlParameter> ListParameter = new List<NpgsqlParameter>();
+
+                Parameter = new NpgsqlParameter("iid", NpgsqlDbType.Bigint);
+                Parameter.Value = iid;
+                ListParameter.Add(Parameter);
+
+                Parameter = new NpgsqlParameter("iname", NpgsqlDbType.Varchar);
+                Parameter.Value = iname;
+                ListParameter.Add(Parameter);
+
+                Parameter = new NpgsqlParameter("idesc", NpgsqlDbType.Varchar);
+                Parameter.Value = idesc;
+                ListParameter.Add(Parameter);
+
+                Parameter = new NpgsqlParameter("isort", NpgsqlDbType.Integer);
+                Parameter.Value = isort;
+                ListParameter.Add(Parameter);
+
+                return ListParameter;
+            }
+        }
+
+        /// <summary>
+        /// Наименование функции API
+        /// </summary>
+        public string NameFunction
+        {
+            get
+            {
+                return "position_upd";
+            }
+        }
+    }
+}
