5a148970aa429e5c38b538e11d56963338e46b42
Ольга Майорова <kitty_O_O@mail.ru>
2023-02-08 17:14:27 +0500

    feat: Добавлены команды выборки позиций

---
+++ command/position/sel/position_by_id.cs
@@ -0,0 +1,53 @@
+﻿using ManDI.command.common;
+using Npgsql;
+using NpgsqlTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManDI.command.position_by_id
+{
+    /// <summary>
+    /// Команда возвращает позицию по идентификатору
+    /// </summary>
+    public class position_by_id : IParametersFunction
+    {
+        /// <summary>
+        /// Идентификатор  позиции
+        /// </summary>
+        public long iid { get; set; }
+
+        
+        /// <summary>
+        /// Список параметров функции
+        /// </summary>
+        public IEnumerable<NpgsqlParameter> Parameters
+        {
+            get
+            {
+                NpgsqlParameter Parameter;
+                List<NpgsqlParameter> ListParameter = new List<NpgsqlParameter>();
+
+                Parameter = new NpgsqlParameter("iid", NpgsqlDbType.Bigint);
+                Parameter.Value = iid;
+                ListParameter.Add(Parameter);
+
+                return ListParameter;
+            }
+        }
+
+        /// <summary>
+        /// Наименование функции API
+        /// </summary>
+        public string NameFunction
+        {
+            get
+            {
+                return "position_by_id";
+            }
+        }
+    }
+}
---
+++ command/position/sel/position_by_id_global_prop.cs
@@ -0,0 +1,52 @@
+﻿using ManDI.command.common;
+using Npgsql;
+using NpgsqlTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManDI.command.position_by_id_global_prop
+{
+    /// <summary>
+    /// Команда возвращает список позиций по идентификатору глобального свойства
+    /// </summary>
+    public class position_by_id_global_prop : IParametersFunction
+    {
+        /// <summary>
+        /// Идентификатор  глобального свойства позиции
+        /// </summary>
+        public long iid_global_prop { get; set; }
+
+        /// <summary>
+        /// Список параметров функции
+        /// </summary>
+        public IEnumerable<NpgsqlParameter> Parameters
+        {
+            get
+            {
+                NpgsqlParameter Parameter;
+                List<NpgsqlParameter> ListParameter = new List<NpgsqlParameter>();
+
+                Parameter = new NpgsqlParameter("iid_global_prop", NpgsqlDbType.Bigint);
+                Parameter.Value = iid_global_prop;
+                ListParameter.Add(Parameter);
+
+                return ListParameter;
+            }
+        }
+
+        /// <summary>
+        /// Наименование функции API
+        /// </summary>
+        public string NameFunction
+        {
+            get
+            {
+                return "position_by_id_global_prop";
+            }
+        }
+    }
+}
