9c6e35187e3ec625a772edf2a0070f5295c793c0
Dmitry Ivanov <firstdismay@gmail.com>
2023-02-15 15:28:50 +0500

    feat: Добавить классы композитных типов измеряемых величин и правил пересчета

---
+++ composite/entities/vunits/vunit_conversion_rules.cs
@@ -0,0 +1,28 @@
+﻿using ManDI.composite.entities.vclass.property;
+
+namespace ManDI.composite.entities.vunits
+{
+    /// <summary>
+    /// Композитный тип правил пересчета
+    /// </summary>
+    public class vunit_conversion_rules
+    {
+        public int id { get; set; }
+        public long id_conception { get; set; }
+        public decimal mc { get; set; }
+        public string bunit { get; set; }
+        public string cunit { get; set; }
+        public string desc { get; set; }
+        public int id_unit { get; set; }
+        public bool is_entity { get; set; }
+        public bool is_use { get; set; }
+        public bool not_fractional { get; set; }
+        public bool on { get; set; }
+        public bool on_base { get; set; }
+        public bool on_single { get; set; }
+        public int round { get; set; }
+        public DateTime timestamp { get; set; }
+    }
+}
+
+
