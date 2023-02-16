55791e8ddade94aecf07e0c54b3a40f2c3245c37
Ольга Майорова <kitty_O_O@mail.ru>
2023-02-16 17:18:50 +0500

    feat: Добавить функции выборки позиций и свойств позиций

---
+++ command/properties/position/sel/position_prop_link_val_by_id_prop.cs
@@ -0,0 +1,61 @@
+﻿using ManDI.command.common;
+using Npgsql;
+using NpgsqlTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManDI.command.properties.position.sel
+{
+    /// <summary>
+    /// Команда возвращает список данных значения свойства позиции LINK STEP №3 по идентификатору свойства
+    /// </summary>
+    public class position_prop_link_val_by_id_prop : IParametersFunction
+    {
+        /// <summary>
+        /// Идентификатор позиции
+        /// </summary>
+        public long iid_position { get; set; }
+
+        /// <summary>
+        /// Идентификатор свойства шаблона позиции
+        /// </summary>
+        public long iid_pos_temp_prop { get; set; }
+
+        /// <summary>
+        /// Список параметров функции
+        /// </summary>
+        public IEnumerable<NpgsqlParameter> Parameters
+        {
+            get
+            {
+                NpgsqlParameter Parameter;
+                List<NpgsqlParameter> ListParameter = new List<NpgsqlParameter>();
+
+                Parameter = new NpgsqlParameter("iid_position", NpgsqlDbType.Bigint);
+                Parameter.Value = iid_position;
+                ListParameter.Add(Parameter);
+
+                Parameter = new NpgsqlParameter("iid_pos_temp_prop", NpgsqlDbType.Bigint);
+                Parameter.Value = iid_pos_temp_prop;
+                ListParameter.Add(Parameter);
+
+                return ListParameter;
+            }
+        }
+
+        /// <summary>
+        /// Наименование функции API
+        /// </summary>
+        public string NameFunction
+        {
+            get
+            {
+                return "position_prop_link_val_by_id_prop";
+            }
+        }
+    }
+}
---
+++ command/properties/position/upd/position_prop_object_val_add_new.cs
@@ -0,0 +1,87 @@
+﻿using ManDI.command.common;
+using Npgsql;
+using NpgsqlTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManDI.command.properties.position.upd
+{
+    /// <summary>
+    /// Команда создает встроенный объект значение объектного свойства STEP №03
+    /// </summary>
+    public class position_prop_object_val_add_new : IParametersFunction
+    {
+        /// <summary>
+        /// Идентификатор позиции
+        /// </summary>
+        public long iid_position_carrier { get; set; }
+
+        /// <summary>
+        /// Идентификатор свойства шаблона позиции
+        /// </summary>
+        public long iid_pos_temp_prop { get; set; }
+
+        /// <summary>
+        ///  Идентификатор класса
+        public long iid_class_real { get; set; }
+
+        /// <summary>
+        ///  Идентификатор правила пересчета
+        public long iid_unit_conversion_rule { get; set; }
+
+        /// <summary>
+        ///  Идентификатор текущего количества единиц
+        /// </sum
+        /// mary>
+        public decimal icquantity { get; set; }
+
+        /// <summary>
+        /// Список параметров функции
+        /// </summary>
+        public IEnumerable<NpgsqlParameter> Parameters
+        {
+            get
+            {
+                NpgsqlParameter Parameter;
+                List<NpgsqlParameter> ListParameter = new List<NpgsqlParameter>();
+
+                Parameter = new NpgsqlParameter("iid_position_carrier", NpgsqlDbType.Bigint);
+                Parameter.Value = iid_position_carrier;
+                ListParameter.Add(Parameter);
+
+                Parameter = new NpgsqlParameter("iid_pos_temp_prop", NpgsqlDbType.Bigint);
+                Parameter.Value = iid_pos_temp_prop;
+                ListParameter.Add(Parameter);
+
+                Parameter = new NpgsqlParameter("iid_class_real", NpgsqlDbType.Bigint);
+                Parameter.Value = iid_class_real;
+                ListParameter.Add(Parameter);
+
+                Parameter = new NpgsqlParameter("iid_unit_conversion_rule", NpgsqlDbType.Integer);
+                Parameter.Value = iid_unit_conversion_rule;
+                ListParameter.Add(Parameter);
+
+                Parameter = new NpgsqlParameter("icquantity", NpgsqlDbType.Numeric);
+                Parameter.Value = icquantity;
+                ListParameter.Add(Parameter);
+
+                return ListParameter;
+            }
+        }
+
+        /// <summary>
+        /// Наименование функции API
+        /// </summary>
+        public string NameFunction
+        {
+            get
+            {
+                return "position_prop_object_val_add_new";
+            }
+        }
+    }
+}
