ff1a553b2b4a1ce03a98adb5ab0d9c3b87f1a013
Dmitry Ivanov <firstdismay@gmail.com>
2023-02-27 14:28:17 +0500

    fix: Включить все доступные композитные типы проекта в класс сборки композитных типов

--- configuration/mapcomposite_config.cs
+++ configuration/mapcomposite_config.cs
@@ -1,13 +1,15 @@
 ﻿using Npgsql;
 using ManDI.composite.entities.common;
+using ManDI.composite.entities.vclass;
+using ManDI.composite.entities.vclass.property;
+using ManDI.composite.entities.vpos_temp;
+using ManDI.composite.entities.vpos_temp.property;
+using ManDI.composite.entities.vposition;
+using ManDI.composite.entities.vposition.property;
 using ManDI.composite.entities.vobject;
 using ManDI.composite.entities.vobject.property;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace ManDI
 {
@@ -23,9 +25,40 @@ namespace ManDI
         {
             dataSourceBuilder.MapComposite<vconception>("vconception");
 
-            dataSourceBuilder.MapComposite<vobject_prop>("vobject_prop");
+            dataSourceBuilder.MapComposite<vgroup>("vgroup");
+            dataSourceBuilder.MapComposite<vclass>("vclass");
+            dataSourceBuilder.MapComposite<vclass_ext>("vclass_ext");
+            dataSourceBuilder.MapComposite<vclass_prop>("vclass_prop");
+            dataSourceBuilder.MapComposite<vclass_prop_enum_val>("vclass_prop_enum_val");
+            dataSourceBuilder.MapComposite<vclass_prop_link_val>("vclass_prop_link_val");
+            dataSourceBuilder.MapComposite<vclass_prop_object_val>("vclass_prop_object_val");
+            dataSourceBuilder.MapComposite<vclass_prop_user_big_val>("vclass_prop_user_big_val");
+            dataSourceBuilder.MapComposite<vclass_prop_user_small_val>("vclass_prop_user_small_val");
+
+            dataSourceBuilder.MapComposite<vpos_temp>("vpos_temp");
+            dataSourceBuilder.MapComposite<vpos_temp_prop>("vpos_temp_prop");
+            dataSourceBuilder.MapComposite<vpos_temp_prop_enum_val>("vpos_temp_prop_enum_val");
+            dataSourceBuilder.MapComposite<vpos_temp_prop_link_val>("vpos_temp_prop_link_val");
+            dataSourceBuilder.MapComposite<vpos_temp_prop_object_val>("vpos_temp_prop_object_val");
+            dataSourceBuilder.MapComposite<vpos_temp_prop_user_big_val>("vpos_temp_prop_user_big_val");
+            dataSourceBuilder.MapComposite<vpos_temp_prop_user_small_val>("vpos_temp_prop_user_small_val");
+
+            dataSourceBuilder.MapComposite<vposition>("vposition");
+            dataSourceBuilder.MapComposite<vposition_prop>("vposition_prop");
+            dataSourceBuilder.MapComposite<vposition_prop_enum_val>("vposition_prop_enum_val");
+            dataSourceBuilder.MapComposite<vposition_prop_link_val>("vposition_prop_link_val");
+            dataSourceBuilder.MapComposite<vposition_prop_object_val>("vposition_prop_object_val");
+            dataSourceBuilder.MapComposite<vposition_prop_user_big_val>("vposition_prop_user_big_val");
+            dataSourceBuilder.MapComposite<vposition_prop_user_small_val>("vposition_prop_user_small_val");
+
             dataSourceBuilder.MapComposite<vobject_general>("vobject_general");
             dataSourceBuilder.MapComposite<vobject_general_ext>("vobject_general_ext");
+            dataSourceBuilder.MapComposite<vobject_prop>("vobject_prop");
+            dataSourceBuilder.MapComposite<vobject_prop_enum_val>("vobject_prop_enum_val");
+            dataSourceBuilder.MapComposite<vobject_prop_link_val>("vobject_prop_link_val");
+            dataSourceBuilder.MapComposite<vobject_prop_object_val>("vobject_prop_object_val");
+            dataSourceBuilder.MapComposite<vobject_prop_user_big_val>("vobject_prop_user_big_val");
+            dataSourceBuilder.MapComposite<vobject_prop_user_small_val>("vobject_prop_user_small_val");
         }
     }
 }
