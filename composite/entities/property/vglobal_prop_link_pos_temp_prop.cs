06d770a3448a6cf0baba908cc624031dc47c4c20
Dmitry Ivanov <firstdismay@gmail.com>
2023-03-21 14:17:09 +0500

    feat: Добавить композитные типы глобальных свойств

---
+++ composite/entities/property/vglobal_prop_link_pos_temp_prop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ManDI.composite.entities.property
+{
+    /// <summary>
+    /// Композитный тип ссылки глобального свойства на свойство шаблона позиций
+    /// </summary>
+    public class vglobal_prop_link_pos_temp_prop
+    {
+        public long id_conception { get; set; }
+        public long id_global_prop { get; set; }
+        public long id_pos_temp { get; set; }
+        public long id_pos_temp_prop { get; set; }
+        public int id_prop_type { get; set; }
+        public int id_data_type { get; set; }
+        public string name { get; set; }
+        public string desc { get; set; }
+        public string name_link { get; set; }
+    }
+}
