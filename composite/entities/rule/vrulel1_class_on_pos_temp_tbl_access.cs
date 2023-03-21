8e9683ff67e56d284dcc4b30a2078ac99c23718b
Dmitry Ivanov <firstdismay@gmail.com>
2023-03-21 10:43:06 +0500

    feat: Добавить классы композитных типов для правил вложенности и разрешений

---
+++ composite/entities/rule/vrulel1_class_on_pos_temp_tbl_access.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ManDI.composite.entities.rule
+{
+    /// <summary>
+    /// Композитный тип разрешений правила вложенности уровня 1 класс на шаблон
+    /// </summary>
+    public class vrulel1_class_on_pos_temp_tbl_access
+    {
+        public long id_con { get; set; }
+        public long id_group { get; set; }
+        public long id_pos_temp { get; set; }
+        public long id_class { get; set; }
+        public bool on_assigned { get; set; }
+        public long id_class_assigned { get; set; }
+        public int class_level { get; set; }
+        public string name_rule { get; set; }
+    }
+}
---
+++ composite/entities/rule/vrulel1_group_on_pos_temp.cs
@@ -0,0 +1,16 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace ManDI.composite.entities.rule
+{
+    /// <summary>
+    /// Композитный тип правила вложенности уровня 1 группа на шаблон
+    /// </summary>
+    public class vrulel1_group_on_pos_temp
+    {
+        public long id_group { get; set; }
+        public long id_pos_temp { get; set; }
+        public string name_rule { get; set; }
+        public bool is_create { get; set; }
+        public bool is_use { get; set; }
+    }
+}
