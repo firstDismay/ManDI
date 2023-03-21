2a782ee95957f6a554293ca08b5e0060097835e3
Dmitry Ivanov <firstdismay@gmail.com>
2023-03-21 14:16:10 +0500

    feat: Добавить композитные типы библиотеки документов

---
+++ composite/entities/document/vdoc_category.cs
@@ -0,0 +1,18 @@
+﻿namespace ManDI.composite.entities.document
+{
+    /// <summary>
+    /// Композитный тип категории документов
+    /// </summary>
+    public class vdoc_category
+    {
+        public long id { get; set; }
+        public long id_conception { get; set; }
+        public string name { get; set; }
+        public string desc { get; set; }
+        public bool on { get; set; }
+        public bool on_grouping { get; set; }
+        public long doc_count { get; set; }
+        public bool is_use { get; set; }
+        public DateTime timestamp { get; set; }
+    }
+}
---
+++ composite/entities/document/vdocument.cs
@@ -0,0 +1,26 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ManDI.composite.entities.document
+{
+    /// <summary>
+    /// Композитный тип документа
+    /// </summary>
+    public class vdocument
+    {
+        public long id { get; set; }
+        public long id_conception { get; set; }
+        public long id_parent { get; set; }
+        public long id_category { get; set; }
+        public bool on_grouping { get; set; }
+        public bool package_item { get; set; }
+        public string name { get; set; }
+        public string desc { get; set; }
+        public string regnum { get; set; }
+        public DateTime regdate { get; set; }
+        public bool has_link { get; set; }
+        public bool has_doc { get; set; }
+        public bool has_file { get; set; }
+        public DateTime timestamp { get; set; }
+        public bool is_inside { get; set; }
+    }
+}
