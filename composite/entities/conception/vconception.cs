0ce7464f59487c128d9685cc4d6a5bd2586235f5
Dmitry Ivanov <firstdismay@gmail.com>
2023-03-21 14:20:14 +0500

    feat: Добавить композитный тип концепции

---
+++ composite/entities/conception/vconception.cs
@@ -0,0 +1,24 @@
+﻿using ManDI.composite.entities.vclass.property;
+
+namespace ManDI.composite.entities.conception
+{
+    /// <summary>
+    /// Композитный тип измеряемых величин
+    /// </summary>
+    public class vconception
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string desc { get; set; }
+        public bool on { get; set; }
+        public bool on_root_create { get; set; }
+        public bool @default { get; set; }
+        public long pos_recycle { get; set; }
+        public long group_recycle { get; set; }
+        public long pos_temp_recycle { get; set; }
+        public int actcatalog { get; set; }
+        public DateTime timestamp { get; set; }
+    }
+}
+
+
