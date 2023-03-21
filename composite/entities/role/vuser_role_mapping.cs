41b298a5c37a8387c0b2049b29ca61945224b6b6
Dmitry Ivanov <firstdismay@gmail.com>
2023-03-21 14:19:14 +0500

    feat: Добавить композитные типы ролевой модели базы

---
+++ composite/entities/role/vuser_role_mapping.cs
@@ -0,0 +1,12 @@
+﻿namespace ManDI.composite.entities.role
+{
+    /// <summary>
+    /// Композитный тип подключаемых ролей пользователя
+    /// </summary>
+    public class vuser_role_mapping
+    {
+        public long id_conception { get; set; }
+        public uint oid_role { get; set; }
+        public string rolname { get; set; }
+    }
+}
