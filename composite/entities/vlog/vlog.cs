97a8c5f8fb358eb2cd4118924f7e6736d97d28d1
Dmitry Ivanov <firstdismay@gmail.com>
2023-03-21 14:20:44 +0500

    feat: Добавить композитные типы журнала

---
+++ composite/entities/vlog/vlog.cs
@@ -0,0 +1,24 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System;
+namespace ManDI.composite.entities.vlog
+{
+    /// <summary>
+    /// Композитный тип записи журнала
+    /// </summary>
+    public class vlog
+    {
+        public long id { get; set; }
+        public long id_conception { get; set; }
+        public string user_current { get; set; }
+        public DateTime timestamp { get; set; }
+        public long id_category { get; set; }
+        public string name_category { get; set; }
+        public int level_category { get; set; }
+        public string title { get; set; }
+        public string message { get; set; }
+        public string class_body { get; set; }
+        public string body { get; set; }
+        public string user_author { get; set; }
+        public DateTime datetime { get; set; }
+    }
+}
---
+++ composite/entities/vlog/vlog_link.cs
@@ -0,0 +1,18 @@
+﻿namespace ManDI.composite.entities.group
+{
+    /// <summary>
+    /// Композитный тип ссылки записи журнала
+    /// </summary>
+    public class vlog_link
+    {
+        public long id { get; set; }
+        public long id_conception { get; set; }
+        public long id_log { get; set; }
+        public long id_category { get; set; }
+        public string message { get; set; }
+        public DateTime datetime { get; set; }
+        public int id_entity { get; set; }
+        public long id_entity_instance { get; set; }
+        public long id_sub_entity_instance { get; set; }
+    }
+}
