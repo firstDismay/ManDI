48dd6c9ddbe864a9d72e8ff8416f24caa2a4959e
Dmitry Ivanov <firstdismay@gmail.com>
2023-03-21 10:44:10 +0500

    faet: Добавить композитные типы для измеряемых величин и правил пересчета

---
+++ composite/entities/unit/vclass_unit_conversion_rules.cs
@@ -0,0 +1,20 @@
+﻿using ManDI.composite.entities.vclass.property;
+
+namespace ManDI.composite.entities.unit
+{
+    /// <summary>
+    /// Композитный тип правил пересчета классов
+    /// </summary>
+    public class vclass_unit_conversion_rules
+    {
+        public long id_class { get; set; }
+        public long id_conception { get; set; }
+        public int id_unit { get; set; }
+        public int id_unit_conversion_rule { get; set; }
+        public bool is_create { get; set; }
+        public bool is_use { get; set; }
+        public bool on_base { get; set; }
+    }
+}
+
+
---
+++ composite/entities/unit/vunits.cs
@@ -0,0 +1,17 @@
+﻿using ManDI.composite.entities.vclass.property;
+using System;
+
+namespace ManDI.composite.entities.unit
+{
+    /// <summary>
+    /// Композитный тип измеряемых величин
+    /// </summary>
+    public class vunits
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string desc { get; set; }
+        public bool is_entity { get; set; }
+        public bool not_fractional { get; set; }
+    }
+}
