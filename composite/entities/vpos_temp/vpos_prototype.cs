ab3bab4ea63e71da9599198e9f51db12aa4763c9
Dmitry Ivanov <firstdismay@gmail.com>
2023-03-21 14:21:40 +0500

    feat: Добавить пропущенные композитные типы для шаблонов позиций и правил вложенности

---
+++ composite/entities/vpos_temp/vpos_prototype.cs
@@ -0,0 +1,17 @@
+﻿namespace ManDI.composite.entities.vpos_temp
+{
+    /// <summary>
+    /// Композитный тип прототипа шаблона позиции
+    /// </summary>
+    public class vpos_prototype
+    {
+        public int id { get; set; }
+        public bool on_root { get; set; }
+        public bool on_this { get; set; }
+        public bool on_pos { get; set; }
+        public bool on_obj { get; set; }
+        public string desc { get; set; }
+        public int sort { get; set; }
+        public bool on_service { get; set; }
+    }
+}
