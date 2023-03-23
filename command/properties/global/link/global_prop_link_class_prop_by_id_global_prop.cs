c6de9079dfb9eb40a0a9dd5ce6c66a50ac965b9f
Dmitry Ivanov <firstdismay@gmail.com>
2023-03-23 11:01:48 +0500

    feat: Добавить команды управления глобальными свойствами

---
+++ command/properties/global/link/global_prop_link_class_prop_by_id_global_prop.cs
@@ -0,0 +1,46 @@
+﻿using Npgsql;
+using NpgsqlTypes;
+
+namespace ManDI.command.properties.global.link
+{
+    /// <summary>
+    /// Команда возвращает список ссылок глобального свойств на определяющие 
+    /// свойства класса по идентификатору глобального свойства
+    /// </summary>
+    public class global_prop_link_class_prop_by_id_global_prop : IParametersFunction
+    {
+        /// <summary>
+        /// Идентификатор глобального свойства
+        /// </summary>
+        public long iid_global_prop { get; set; }
+
+        /// <summary>
+        /// Список параметров функции
+        /// </summary>
+        public IEnumerable<NpgsqlParameter> Parameters
+        {
+            get
+            {
+                NpgsqlParameter Parameter;
+                List<NpgsqlParameter> ListParameter = new List<NpgsqlParameter>();
+
+                Parameter = new NpgsqlParameter("iid_global_prop", NpgsqlDbType.Bigint);
+                Parameter.Value = iid_global_prop;
+                ListParameter.Add(Parameter);
+
+                return ListParameter;
+            }
+        }
+
+        /// <summary>
+        /// Наименование функции API
+        /// </summary>
+        public string NameFunction
+        {
+            get
+            {
+                return "global_prop_link_class_prop_by_id_global_prop";
+            }
+        }
+    }
+}
---
+++ command/properties/global/upd/global_prop_upd.cs
@@ -0,0 +1,90 @@
+﻿using Npgsql;
+using NpgsqlTypes;
+
+namespace ManDI.command.properties.global.upd
+{
+    /// <summary>
+    /// Команда обновляет глобальное свойство концепции
+    /// </summary>
+    public class global_prop_upd : IParametersFunction
+    {
+        /// <summary>
+        /// Идентификатор глобального свойства
+        /// </summary>
+        public long iid_global_prop { get; set; }
+
+        /// <summary>
+        /// Идентификатор типа свойства
+        /// </summary>
+        public int iid_prop_type { get; set; }
+
+        /// <summary>
+        /// Идентификатор типа данных свойства
+        /// </summary>
+        public int iid_data_type { get; set; }
+
+        /// <summary>
+        /// Наименование свойства
+        /// </summary>
+        public string iname { get; set; }
+
+        /// <summary>
+        /// Описание свойства
+        /// </summary>
+        public string idesc { get; set; }
+
+        /// <summary>
+        /// Признак видимости свойства
+        /// </summary>
+        public string ivisible { get; set; }
+
+        /// <summary>
+        /// Список параметров функции
+        /// </summary>
+        public IEnumerable<NpgsqlParameter> Parameters
+        {
+            get
+            {
+                NpgsqlParameter Parameter;
+                List<NpgsqlParameter> ListParameter = new List<NpgsqlParameter>();
+
+                Parameter = new NpgsqlParameter("iid_global_prop", NpgsqlDbType.Bigint);
+                Parameter.Value = iid_global_prop;
+                ListParameter.Add(Parameter);
+
+                Parameter = new NpgsqlParameter("iid_prop_type", NpgsqlDbType.Integer);
+                Parameter.Value = iid_prop_type;
+                ListParameter.Add(Parameter);
+
+                Parameter = new NpgsqlParameter("iid_data_type", NpgsqlDbType.Integer);
+                Parameter.Value = iid_data_type;
+                ListParameter.Add(Parameter);
+
+                Parameter = new NpgsqlParameter("iname", NpgsqlDbType.Varchar);
+                Parameter.Value = iname;
+                ListParameter.Add(Parameter);
+
+                Parameter = new NpgsqlParameter("idesc", NpgsqlDbType.Varchar);
+                Parameter.Value = idesc;
+                ListParameter.Add(Parameter);
+
+                Parameter = new NpgsqlParameter("ivisible", NpgsqlDbType.Boolean);
+                Parameter.Value = ivisible;
+                ListParameter.Add(Parameter);
+
+                return ListParameter;
+            }
+        }
+
+        /// <summary>
+        /// Наименование функции API
+        /// </summary>
+        public string NameFunction
+        {
+            get
+            {
+                return "global_prop_upd";
+            }
+        }
+    }
+}
