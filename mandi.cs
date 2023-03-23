6d3d34f326a1e921a15464e0f610a5cc2473ca43
Dmitry Ivanov <firstdismay@gmail.com>
2023-03-23 11:58:19 +0500

    fix: Исправить структуру сборщика проекта ManDI

--- mandi.cs
+++ mandi.cs
@@ -13,24 +13,34 @@ namespace ManDI
     /// <summary>
     /// Базовый конфигуратор источника данных ManDI
     /// </summary>
-    public partial class ManDiBuilder
+    public partial class mandi
     {
-        NpgsqlConnectionStringBuilder _csb;
-        public ManDiBuilder(NpgsqlConnectionStringBuilder ConnectionStringBuilder)
+        NpgsqlConnectionStringBuilder csb;
+        NpgsqlDataSource data_source;
+        public mandi(NpgsqlConnectionStringBuilder ConnectionStringBuilder)
         {
             if (ConnectionStringBuilder == null) throw new ArgumentNullException("ConnectionStringBuilder");
-            _csb = ConnectionStringBuilder;
+            this.csb = ConnectionStringBuilder;
         }
         /// <summary>
         /// Сборка источника данных ManDI
         /// </summary>
         public NpgsqlDataSource Build()
         {
-            ConnectionStringConfiguration(_csb);
-            var dataSourceBuilder = new NpgsqlDataSourceBuilder(_csb.ConnectionString);
+            ConnectionStringConfiguration(this.csb);
+            var dataSourceBuilder = new NpgsqlDataSourceBuilder(this.csb.ConnectionString);
             MapComposite(dataSourceBuilder);
             MapEnum(dataSourceBuilder);
             return dataSourceBuilder.Build();
+        }
+
+        public NpgsqlDataSource GetDataSource()
+        {
+            if (this.data_source == null)
+            {
+                this.data_source = this.Build();
+            }
+            return this.data_source;
         }
     }
 }
