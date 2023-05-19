e7440692735bd9e90f20c27cb59469f263e8b857
Dmitry Ivanov <firstdismay@gmail.com>
2023-05-19 18:05:14 +0500

    refact: Перенести метод сборки зависимостей ManDI в раздел build проекта ManDI

---
+++ build/ManDIServiceCollectionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using ManDI;
+using ManDI.build;
+using ManDI.enums;
+using ManDI.executor;
+using ManDI.extractor;
+using Npgsql;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebClient.build
+{
+    /// <summary>
+    /// Сборка сервисов доступа к данным
+    /// </summary>
+    public static class ManDIServiceCollectionExtensions
+    {
+        public static IServiceCollection AddManDI(this IServiceCollection services, ManDiOptions options)
+        {
+            NpgsqlConnectionStringBuilder csb = new NpgsqlConnectionStringBuilder(options.ConnectionStrings);
+
+            services.Add(new ServiceDescriptor(typeof(NpgsqlConnectionStringBuilder), csb));
+            services.Add(new ServiceDescriptor(typeof(IDataService), typeof(ManDiDataService), ServiceLifetime.Scoped));
+            if (options != null)
+            {
+                switch (options.SignatureExtractorMode)
+                {
+                    case eSignatureExtractorMode.SignatureExtractorForComposite:
+                        services.Add(new ServiceDescriptor(typeof(ISignatureExtractor), typeof(SignatureExtractorForComposite), ServiceLifetime.Scoped));
+                        break;
+                    case eSignatureExtractorMode.SignatureExtractorForTable:
+                        services.Add(new ServiceDescriptor(typeof(ISignatureExtractor), typeof(SignatureExtractorForTable), ServiceLifetime.Scoped));
+                        break;
+                }
+            }
+            else
+            {
+                services.Add(new ServiceDescriptor(typeof(ISignatureExtractor), typeof(SignatureExtractorForComposite), ServiceLifetime.Scoped));
+            }
+            services.Add(new ServiceDescriptor(typeof(ICommandExecutor), typeof(command_executor), ServiceLifetime.Scoped));
+
+            return services;
+        }
+    }
+}
