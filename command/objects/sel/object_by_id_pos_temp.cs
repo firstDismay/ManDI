109e729b011b93914087e7f444080e1d34eee9c9
Dmitry Ivanov <firstdismay@gmail.com>
2023-05-22 16:29:11 +0500

    feat: Добавить команды выбора объектов по идентификатору шаблона позиции object_by_id_pos_temp и object_ext_by_id_pos_temp

---
+++ command/objects/sel/object_by_id_pos_temp.cs
@@ -0,0 +1,44 @@
+﻿using Npgsql;
+using NpgsqlTypes;
+
+namespace ManDI.command.objects.sel
+{
+    /// <summary>
+    /// Команда возвращает список объектов шаблона позиции по идентификатору шаблона позиции
+    /// </summary>
+    public class object_by_id_pos_temp : IParametersFunction
+    {
+        /// <summary>
+        /// Идентификатор шаблона позиции объекта
+        /// </summary>
+        public long iid_pos_temp { get; set; }
+
+        /// <summary>
+        /// Список параметров функции
+        /// </summary>
+        public IEnumerable<NpgsqlParameter> Parameters
+        {
+            get
+            {
+                NpgsqlParameter Parameter;
+                List<NpgsqlParameter> ListParameter = new List<NpgsqlParameter>();
+
+                Parameter = new NpgsqlParameter("iid_pos_temp", NpgsqlDbType.Bigint);
+                Parameter.Value = iid_pos_temp;
+                ListParameter.Add(Parameter);
+                return ListParameter;
+            }
+        }
+
+        /// <summary>
+        /// Наименование функции API
+        /// </summary>
+        public string NameFunction
+        {
+            get
+            {
+                return "object_by_id_pos_temp";
+            }
+        }
+    }
+}
