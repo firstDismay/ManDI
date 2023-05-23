db2dbba6984edf3dd756b0edc302e8c15b5db080
Dmitry Ivanov <firstdismay@gmail.com>
2023-05-23 14:33:01 +0500

    feat: Добавить команды выборки объектов позиций указанного шаблона рекурсивно

---
+++ command/objects/sel/object_by_id_position_id_pos_temp.cs
@@ -0,0 +1,53 @@
+﻿using Npgsql;
+using NpgsqlTypes;
+
+namespace ManDI.command.objects.sel
+{
+    /// <summary>
+    /// Команда возвращает список объектов позиций указанного шаблона рекурсивно начиная с указанной позиции
+    /// </summary>
+    public class object_by_id_position_id_pos_temp : IParametersFunction
+    {
+        /// <summary>
+        /// Идентификатор стартовой позиции
+        /// </summary>
+        public long iid_position { get; set; }
+
+        /// <summary>
+        /// Идентификатор шаблона позиции
+        /// </summary>
+        public long iid_pos_temp { get; set; }
+
+        /// <summary>
+        /// Список параметров функции
+        /// </summary>
+        public IEnumerable<NpgsqlParameter> Parameters
+        {
+            get
+            {
+                NpgsqlParameter Parameter;
+                List<NpgsqlParameter> ListParameter = new List<NpgsqlParameter>();
+
+                Parameter = new NpgsqlParameter("iid_position", NpgsqlDbType.Bigint);
+                Parameter.Value = iid_position;
+                ListParameter.Add(Parameter);
+
+                Parameter = new NpgsqlParameter("iid_pos_temp", NpgsqlDbType.Bigint);
+                Parameter.Value = iid_position;
+                ListParameter.Add(Parameter);
+                return ListParameter;
+            }
+        }
+
+        /// <summary>
+        /// Наименование функции API
+        /// </summary>
+        public string NameFunction
+        {
+            get
+            {
+                return "object_by_id_position_id_pos_temp";
+            }
+        }
+    }
+}
