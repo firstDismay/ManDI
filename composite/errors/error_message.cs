c1f7e8fe223c2132711683a8bc53ffcc88f21eb3
Dmitry Ivanov <firstdismay@gmail.com>
2023-07-11 15:40:59 +0500

    refact: Убрать тип сообщений об ошибке добавления объектов. Добавить общий тип  сообщений об ошибках

---
+++ composite/errors/error_message.cs
@@ -0,0 +1,60 @@
+﻿using Newtonsoft.Json;
+using pg_class.pg_exceptions;
+
+namespace ManDI.composite.errors
+{
+    /// <summary>
+    /// Квантовый класс для возврата результатов групповых операций 
+    /// </summary>
+    public class error_message
+    {
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        public error_message() { }
+
+        /// <summary>
+        /// Полный конструктор класса для возврата данных существующих записей через строку таблицы 
+        /// </summary>
+        public error_message(System.Data.DataRow row) : this()
+        {
+            if (row.Table.TableName == "error_message")
+            {
+                message = (String)row["message"];
+                entity_id = (Int32)row["entity_id"];
+                entity_instance_id = (Int64)row["entity_instance_id"];
+                sub_entity_instance_id = (Int64)row["sub_entity_instance_id"];
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException(String.Format("Наименование входной таблицы '{0}' не соответствует ограничениям конструктора!", row.Table.TableName));
+            }
+        }
+
+        /// <summary>
+        /// Сообщение об ошибке в формате JSON (PgFunctionMessage)
+        /// </summary>
+        public String message { get; set; }
+
+        /// <summary>
+        /// Идентификатор сущности
+        /// </summary>
+        public Int32 entity_id { get; set; }
+
+        /// <summary>
+        /// Идентификатор экземпляра сущности
+        /// </summary>
+        public Int64 entity_instance_id { get; set; }
+
+        /// <summary>
+        /// Идентификатор сущности дополнительный
+        /// </summary>
+        public Int64 sub_entity_instance_id { get; set; }
+
+
+        public PgFunctionMessage MessageGet()
+        {
+            return JsonConvert.DeserializeObject<PgFunctionMessage>(message);
+        }
+    }
+}
---
+++ exception/PgFunctionMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace pg_class.pg_exceptions
+{
+    /// <summary>
+    /// Сообщение функции API
+    /// </summary>
+    public class PgFunctionMessage
+    {
+        public String func { get; set; }
+        public String codeerr { get; set; }
+        public String actionerr { get; set; }
+        public String entity { get; set; }
+        public String messageerr { get; set; }
+        public String hinterr { get; set; }
+        public String classerr { get; set; }
+        public String classerrdesc { get; set; }
+    }
+}
