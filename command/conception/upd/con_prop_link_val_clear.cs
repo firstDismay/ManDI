03d8280386e261069ef1704112ff06c5508fe256
Dmitry Ivanov <firstdismay@gmail.com>
2023-07-19 12:25:36 +0500

    refact: Добавить функцию очистки концепции от мусорных ссылок

---
+++ command/conception/upd/con_prop_link_val_clear.cs
@@ -0,0 +1,47 @@
+﻿using Npgsql;
+using NpgsqlTypes;
+
+namespace ManDI.command.conception.upd
+{
+    /// <summary>
+    /// Команда удаляет данные значения ссылочных свойств, указывающие на недоступные сущности по идентификатору концепции
+    /// <summary>
+    public class con_prop_link_val_clear : IParametersFunction
+    {
+        /// <summary>
+        /// Идентификатор концепции
+        /// </summary>
+        public long iid_conception { get; set; }
+
+        /// <summary>
+        /// Список параметров функции
+        /// </summary>
+        public IEnumerable<NpgsqlParameter> Parameters
+        {
+            get
+            {
+                NpgsqlParameter Parameter;
+                List<NpgsqlParameter> ListParameter = new List<NpgsqlParameter>();
+
+                Parameter = new NpgsqlParameter("iid_conception", NpgsqlDbType.Bigint);
+                Parameter.Value = iid_conception;
+                ListParameter.Add(Parameter);
+
+                return ListParameter;
+            }
+        }
+
+        /// <summary>
+        /// Наименование функции API
+        /// </summary>
+        public string NameFunction
+        {
+            get
+            {
+                return "con_prop_link_val_clear";
+            }
+        }
+
+        public string? Condition { get; set; }
+    }
+}
