fa5f71343948378c4cc4c5b9daa56bd5f0590baa
Dmitry Ivanov <firstdismay@gmail.com>
2024-01-29 14:38:28 +0500

    feat: Добавить команды class_act_base_by_id_conception и class_act_ext_base_by_id_conception

---
+++ command/classes/sel_ext/class_act_ext_base_by_id_conception.cs
@@ -0,0 +1,50 @@
+﻿using Npgsql;
+using NpgsqlTypes;
+
+namespace ManDI.command.classes.sel
+{
+    /// <summary>
+    /// Команда возвращает список активных базовых классов по идентификатору концепции
+    /// </summary>
+    public class class_act_ext_base_by_id_conception : IParametersFunction
+    {
+        /// <summary>
+        /// Идентификатор концепции
+        /// </summary>
+        public long iid_conception { get; set; }
+
+        /// <summary>
+        /// Список параметров функции
+        /// </summary>
+        public IEnumerable<NpgsqlParameter> Parameters
+        {
+            get
+            {
+                NpgsqlParameter Parameter;
+                List<NpgsqlParameter> ListParameter = new List<NpgsqlParameter>();
+
+                Parameter = new NpgsqlParameter("iid_conception", NpgsqlDbType.Bigint);
+                Parameter.Value = iid_conception;
+                ListParameter.Add(Parameter);
+
+                return ListParameter;
+            }
+        }
+
+        /// <summary>
+        /// Наименование функции API
+        /// </summary>
+        public string NameFunction
+        {
+            get
+            {
+                return "class_act_ext_base_by_id_conception";
+            }
+        }
+
+        /// <summary>
+        /// Дополнительные параметры вызова функции добавляемые к основному запросу
+        /// </summary>
+        public string? Condition { get; set; }
+    }
+}
