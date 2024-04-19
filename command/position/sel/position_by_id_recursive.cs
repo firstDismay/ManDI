46832834d3c6a625f1b77cfc1b9756c5e1eb3334
Dmitry Ivanov <firstdismay@gmail.com>
2024-04-19 12:48:20 +0500

    refact: Добавить команду position_by_id_recursive возвращающую список позиций по идентификатору позиции рекурсивно

---
+++ command/position/sel/position_by_id_recursive.cs
@@ -0,0 +1,47 @@
+﻿using Npgsql;
+using NpgsqlTypes;
+
+namespace ManDI.command.position.sel
+{
+    /// <summary>
+    /// Команда возвращает список позиций по идентификатору позиции рекурсивно
+    /// </summary>
+    public class position_by_id_recursive : IParametersFunction
+    {
+        /// <summary>
+        /// Идентификатор  позиции
+        /// </summary>
+        public long iid { get; set; }
+
+        /// <summary>
+        /// Список параметров функции
+        /// </summary>
+        public IEnumerable<NpgsqlParameter> Parameters
+        {
+            get
+            {
+                NpgsqlParameter Parameter;
+                List<NpgsqlParameter> ListParameter = new List<NpgsqlParameter>();
+
+                Parameter = new NpgsqlParameter("iid", NpgsqlDbType.Bigint);
+                Parameter.Value = iid;
+                ListParameter.Add(Parameter);
+
+                return ListParameter;
+            }
+        }
+
+        /// <summary>
+        /// Наименование функции API
+        /// </summary>
+        public string NameFunction
+        {
+            get
+            {
+                return "position_by_id_recursive";
+            }
+        }
+
+        public string? Condition { get; set; }
+    }
+}
